3dd535a391f62835e0315a7e352b9870f5715782
Apedlop <angelapedreralopez@gmail.com>
2024-11-07 11:04:44 +0100

    .

--- Prueba/src/Main.java
+++ Prueba/src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Cliente> listaClientes = new ArrayList<>();
+        Collection<Cliente> colleccionClien = listaClientes;
+
+        Cliente cliente = new Cliente("111", "Sofia", "12/02/1996");
+        colleccionClien.add(cliente);
+        colleccionClien.add(new Cliente("112", "Sofia", "12/02/1996"));
+        colleccionClien.remove(cliente);
+        colleccionClien.clear();
+        colleccionClien.add(new Cliente("113", "Sofia", "12/02/1996"));
+        colleccionClien.add(new Cliente("114", "Sofia", "12/02/1996"));
+        int tamano = colleccionClien.size();
+        System.out.println(tamano);
+        boolean vacio = colleccionClien.isEmpty();
+        System.out.println(vacio);
+        boolean esta = colleccionClien.contains(new Cliente("114", "Sofia", "12/02/1996"));
+        System.out.println(esta);
     }
+
 }
